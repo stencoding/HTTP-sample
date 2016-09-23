97982df0fee95b05256942044dd28c5e7575ea89
ZiperRom1 <romain.laneuville@hotmail.fr>
2016-09-23 10:03:30 +0200

    ~ indent / spacing

--- src/main/main.go
+++ src/main/main.go
@@ -9,11 +9,15 @@ import (
 const addr = "localhost:12345"
 
 func main() {
-	mux := http.NewServeMux()
+	mux     := http.NewServeMux()
 	handler := &myserver.MyHandler{}
+
 	mux.Handle("/favicon.ico", http.NotFoundHandler())
 	mux.Handle("/", handler)
+
 	log.Printf("Now listening on %s...\n", addr)
+
 	server := http.Server{Handler: mux, Addr: addr}
+	
 	log.Fatal(server.ListenAndServe())
 }
